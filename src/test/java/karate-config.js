c33cada447cfa83538f9fcb9da0740f641b70fd9
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-12-30 18:56:35 -0700

    Setting up properties for the project that can be accessible everywhere

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -6,11 +6,21 @@ function fn() {
   }
   var config = {
     env: env,
-	myVarName: 'someValue'
+    spartan_base_url : 'http://52.201.187.226:8000/',
+	b20Slogan  : 'VOILA!!!',
+	b20Slogan2  : 'Nothing is Impossible',
+	b20Slogan3  : 'can you push the code',
+	b20Slogan4  : 'Are we there yet?',
+	b20Slogan5  : 'zero to Hero!!!!',
+	b20Slogan6  : 'Be a good manipulator',
+	b20Slogan7  : 'Theeeeeeeeee Documentation',
+	b20Slogan8  : 'Take it and use it',
+	b20Slogan9  : 'Test test Test',
+	b20Slogan10  : 'Hold your horse',
   }
   if (env == 'dev') {
     // customize
-    // e.g. config.foo = 'bar';
+    config.b20Slogan5 = 'Release the horse';
   } else if (env == 'e2e') {
     // customize
   }
